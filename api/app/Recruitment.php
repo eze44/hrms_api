b70bd442deb7fab618cb0077873a3787dced98df
Betim Sherifi <betimsh99@gmail.com>
2020-06-13 19:38:47 +0200

    show applicants when returning recruitments

--- api/app/Recruitment.php
+++ api/app/Recruitment.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Recruitment extends Model
 {
     protected $table = "recruitment";
+    public $with = ['applicant'];
+
+    public function applicant() {
+        return $this->belongsTo('App\Applicant', 'applicant_id');
+    }
 }
