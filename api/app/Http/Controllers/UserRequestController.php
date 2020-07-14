741a67cfd9a59caf87229c5094f70347dc3a583d
Betim Sherifi <betimsh99@gmail.com>
2020-07-14 19:01:20 +0200

    minor fix

--- api/app/Http/Controllers/UserRequestController.php
+++ api/app/Http/Controllers/UserRequestController.php
@@ -53,7 +53,7 @@ class UserRequestController extends Controller
             $this->userRequestService->insert([
                 'type_id' => $request->input('type_id'),
                 'user_id' => $user->id,
-                'details' => $request->input('type_id'),
+                'details' => $request->input('details'),
                 'status_id' => UserRequestStatus::$PENDING,
                 'date_from' => $request->input('date_to'),
                 'date_to' => $request->input('date_to'),
